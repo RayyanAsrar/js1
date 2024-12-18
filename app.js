c89702974e3876aeca631ac77515ad8468180e1b
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2024-12-18 08:47:49 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//assignment 1
+//q1
+// alert('Welcome to our webiste')
+//q2
+// alert('ERROR! PLEASE ENTER A VALID PASSWORD ')
+//q3
+// alert('Welcome to JS Land \n Happy Coding!')
+// alert('Welcome to JS Land')
+//q4
+// alert('Happy Coding!')
+//q5
+//assingmemt 2
+//q1
+// console.log('Hello... I can run JS through my web browsers console')
+//q2
+// let username = "rayyan"
+//q3
+// let myName = "Muhammad Rayyan Asrar"
+//q4
+// let message = "Hello world"
+// alert(message)
+//q5
+// let studentName="Jhon Doe"
+// let age="15"
+// let skill ="Certified mobile application development"
+// alert(studentName)
+// alert(`my age is ${age   }`)
+// alert(skill)
+//q6
+// let fHeading ="PIZZA"
+// let sHeading ="PIZZ"
+// let tHeading ="PIZ"
+// let frHeading="PI"
+// let  fvHeading="P"
+// alert(`${fHeading} \n ${sHeading}  \n ${tHeading} \n  ${frHeading}  \n  ${fvHeading}`)
+//q7
+
+// let email ="mrayyanasrar@gmail.com"
+// alert(`My email address is ${email}`)
+//q8
+// let bookName="A smaerter way to learn JS"
+// alert(`I am trying to learn from the the ${bookName}`)
+//q9
+// document.write('Yah i can write html content through JS')
+//assisggnment3
+//q1
+// let age = 15
+// alert("I am " + age + " years old")
+//q2
+// let visitCount = 14
+// alert("You have visited this site " + visitCount + " times.")
+//q3
+// document.write("My birth year is " + birthYear + "<br>")
+// document.write("Data type of my declared variable is a number" )
+//q4
+// let visitorName = "John Doe"
+// let productTitle = "T-shirt"
+// let quantity = 5
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.")
